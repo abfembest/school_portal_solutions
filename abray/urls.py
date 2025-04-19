dc66e732e91b91ba61cf29ab0a7cb0af54af3ff4
George <koladegeore5@gmail.com>
2025-04-19 16:44:38 +0100

    URL & pages update

--- abray/urls.py
+++ abray/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('course', views.course, name='course'),
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
+    path('password_reset', views.password_reset, name='password_reset')
 ]
--- abray/views.py
+++ abray/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def course(request):
+    return render(request, 'courses.html')
+
+def register(request):
+    return render(request, 'signup.html')
+
+def login(request):
+    return render(request, 'signin.html')
+
+def password_reset(request):
+    return 'Hi'
